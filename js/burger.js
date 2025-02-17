3a539419feb5d2f2435b07124648de8dca2687bf
bogdankurenkov <badikradik123@gmail.com>
2025-02-17 19:11:51 +0300

    disabled burger menu

--- js/burger.js
+++ js/burger.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.burger-menu');
-    const header = document.querySelector('.header-col3');
-    const close = document.querySelector('.close-submenu');
-    const html = document.documentElement;
-    const body = document.body;
+// document.addEventListener('DOMContentLoaded', () => {
+//     const burger = document.querySelector('.burger-menu');
+//     const header = document.querySelector('.header-col3');
+//     const close = document.querySelector('.close-submenu');
+//     const html = document.documentElement;
+//     const body = document.body;
 
-    burger?.addEventListener('click', () => {
-        header?.classList.add('header-col3_disabled');
-        html.classList.add('body-lock');
-        body.classList.add('body-lock');
-    })
+//     burger?.addEventListener('click', () => {
+//         header?.classList.add('header-col3_disabled');
+//         html.classList.add('body-lock');
+//         body.classList.add('body-lock');
+//     })
 
-    close?.addEventListener('click', () => {
-        header?.classList.remove('header-col3_disabled')
-        html.classList.remove('body-lock');
-        body.classList.remove('body-lock');
+//     close?.addEventListener('click', () => {
+//         header?.classList.remove('header-col3_disabled')
+//         html.classList.remove('body-lock');
+//         body.classList.remove('body-lock');
 
-    })
+//     })
 
-})
+// })
