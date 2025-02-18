afdff7ee33a71b7117591dd198103daee7d34b39
bogdankurenkov <badikradik123@gmail.com>
2025-02-18 12:14:16 +0300

    feat: adede change img logic for map dd

--- js/clubsDd.js
+++ js/clubsDd.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         mapClubs.forEach((club) => {
             club.classList.remove('active');
+            club.querySelector('img').setAttribute('src', '/assets/svg/plus.svg')
         });
     };
 
@@ -27,23 +28,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 closeAllDropdowns();
 
                 if (dropdownContent) {
+                    console.log(1)
                     dropdownContent.classList.add('active');
                 }
                 if (activePreview) {
+                    console.log(2)
                     activePreview.classList.add('club-preview_active');
                 }
                 club.classList.add('active');
+                club.querySelector('img').setAttribute('src', '/assets/svg/minus.svg')
+                console.log(3)
             }
         });
     });
 
     clubPreviews.forEach((preview) => {
         preview.addEventListener('click', (event) => {
-            const clubIndex = preview.classList[1].split('_')[1]; 
+            const clubIndex = preview.classList[1].split('_')[1];
             const dropdownContent = document.querySelector(`.dropdown-content_map[data-club-index="${clubIndex}"]`);
             const activeClub = document.querySelector(`.map-club[data-club-index="${clubIndex}"]`);
 
             if (dropdownContent && dropdownContent.classList.contains('active')) {
+                activeClub.querySelector('img').setAttribute('src', '/assets/svg/plus.svg')
                 closeAllDropdowns();
             } else {
                 closeAllDropdowns();
@@ -53,6 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
                 if (activeClub) {
                     activeClub.classList.add('active');
+                    activeClub.querySelector('img').setAttribute('src', '/assets/svg/minus.svg')
                 }
                 preview.classList.add('club-preview_active');
             }
