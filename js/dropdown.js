22eea867504b7b3f613494d3415b6adda4468d5a
bogdankurenkov <badikradik123@gmail.com>
2025-02-18 10:10:54 +0300

    feat: added valid dates to date select

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,11 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // const datesDd = document.querySelectorAll('#dd-date');
+  const datesDd = document.querySelectorAll('#dd-date');
+  const today = new Date();
 
-  // datesDd.forEach((dateItem) => {
-  //   const span = dateItem.querySelectorAll('span');
-  //   console.log(span)
-  // })
+  const getDateAfterDays = (date, days) => {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  };
+
+  const days = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+
+  const getDayOfWeek = (date) => {
+    console.log(date)
+    return days[date.getDay()];
+  };
+
+  const nextDay1 = getDateAfterDays(today, 1);
+  const nextDay2 = getDateAfterDays(today, 2);
+  const nextDay3 = getDateAfterDays(today, 3);
+
+  console.log(nextDay2)
+
+  const d = today.toLocaleDateString().split('.').slice(0, 2).join('.');
+  const nd = nextDay1.toLocaleDateString().split('.').slice(0, 2).join('.');
+  const nd2 = nextDay2.toLocaleDateString().split('.').slice(0, 2).join('.');
+  const nd3 = nextDay3.toLocaleDateString().split('.').slice(0, 2).join('.');
+  const insert = document.querySelectorAll('#insert');
+
+
+  insert.forEach((item) => {
+    item.textContent = `Сегодня, ${d}`
+  })
+
+
+  datesDd.forEach((dd) => {
+    const dateItems = dd.querySelectorAll('.menu-item_date');
+
+    dateItems.forEach((item, index) => {
+      if (index === 0) {
+        item.setAttribute("data-value", `Сегодня, ${d}`);
+        item.querySelector('span').textContent = d
+      }
+
+      if (index === 1) {
+        item.setAttribute("data-value", `Завтра, ${nd}`);
+        item.querySelector('span').textContent = nd;
+      }
+
+      if (index === 2) {
+        item.setAttribute('data-value', `${getDayOfWeek(nextDay2)}, ${nd2}`)
+        item.querySelector('span').textContent = nd2;
+      }
+      if (index === 3) {
+        item.setAttribute('data-value', `${getDayOfWeek(nextDay3)}, ${nd3}`)
+        item.querySelector('span').textContent = nd3;
+      }
+
+    })
+
+  })
 
   document.addEventListener('click', (event) => {
     const dropdownHeader = event.target.closest('.dropdown-header[data-dropdown]');
