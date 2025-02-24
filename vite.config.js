f3057712ba92062b41054e4ffa036edd5a64f7e8
bogdankurenkov <badikradik123@gmail.com>
2025-02-24 11:28:21 +0300

    fx

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     build: {
-        outDir: 'dist', // Директория вывода после сборки
-        assetsDir: 'assets', // Директория для статических ресурсов
-        emptyOutDir: true, // Очищать директорию вывода перед сборкой
-        rollupOptions: {
-            input: 'index.html' // Главный входной файл
-        }
+        outDir: 'dist',
     },
-    base: './', // Базовый путь для ресурсов
-    server: {
-        port: 3000, // Порт для локального сервера
-        open: true // Автоматически открывать браузер при запуске
-    }
 });
