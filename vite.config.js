7cb61f0f00ae7881640a0679c427274e3415a9ed
Igor Galinovskii <galinovskii95@gmail.com>
2025-02-26 23:43:46 +0300

    added club pages (video, trainers)

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: './',
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-        rollupOptions: {
-            input: {
-                main: 'index.html',
-                clubs: 'clubs.html',
-            },
-        },
+  base: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        clubs: "clubs.html",
+        clubsAtlant: "clubs-atlant.html",
+        clubsAtmosphere: "clubs-atmosphere.html",
+        clubsBirusova: "clubs-birusova.html",
+        clubsHamovniki: "clubs-hamovniki.html",
+        clubsSails: "clubs-sails.html",
+      },
     },
+  },
 });
