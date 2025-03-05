e3e9277780d0a4b36c5d35ea5890dfbc15caaf9f
Igor Galinovskii <galinovskii95@gmail.com>
2025-03-05 13:37:47 +0300

    add: added partner page,  fix  modal.js

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
         fitnes: "fitnes.html",
         servicesDetail: "services-detail.html",
         widget: "widget.html", // test widget
+        partners: "partners.html",
       },
     },
   },
