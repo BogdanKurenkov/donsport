9e5c97facbe8514eaf4b35dbbae883e37ec77954
Igor Galinovskii <galinovskii95@gmail.com>
2025-03-10 20:58:45 +0300

    add: add count suffix

--- js/count.js
+++ js/count.js
@@ -1,58 +1,68 @@
-import { CountUp } from 'countup.js';
+import { CountUp } from "countup.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const counters = document.querySelectorAll('.counter');
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                initializeCountUp(entry.target);
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.5 });
-
-    counters.forEach(counter => {
-        if (isElementInViewport(counter)) {
-            initializeCountUp(counter);
-        } else {
-            observer.observe(counter);
+  const counters = document.querySelectorAll(".counter");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          initializeCountUp(entry.target);
+          observer.unobserve(entry.target);
         }
-    });
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  counters.forEach((counter) => {
+    if (isElementInViewport(counter)) {
+      initializeCountUp(counter);
+    } else {
+      observer.observe(counter);
+    }
+  });
 });
 
 function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
 function initializeCountUp(target) {
-    if (!target || !(target instanceof HTMLElement)) {
-        return;
-    }
+  if (!target || !(target instanceof HTMLElement)) {
+    return;
+  }
 
-    const endValue = parseInt(target.getAttribute('data-target'), 10);
+  const endValue = parseInt(target.getAttribute("data-target"), 10);
+  // add data-suffix
+  const suffix = target.getAttribute("data-suffix") || "";
 
-    if (isNaN(endValue)) {
-        return;
-    }
+  if (isNaN(endValue)) {
+    return;
+  }
 
-    const countUp = new CountUp(target, endValue, {
-        startVal: 0,
-        duration: 2,
-        useEasing: true,
-        useGrouping: false,
-        separator: '',
-        decimal: '.',
-    });
+  const countUp = new CountUp(target, endValue, {
+    startVal: 0,
+    duration: 2,
+    useEasing: true,
+    useGrouping: false,
+    separator: "",
+    decimal: ".",
+    // add format
+    formattingFn: function (n) {
+      return n + suffix;
+    },
+  });
 
-    if (!countUp.error) {
-        countUp.start();
-    } else {
-        console.error(`err: ${countUp.error}`);
-    }
+  if (!countUp.error) {
+    countUp.start();
+  } else {
+    console.error(`err: ${countUp.error}`);
+  }
 }
