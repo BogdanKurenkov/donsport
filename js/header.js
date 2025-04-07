7d78a681b82d98fd672ca1a67ee9dde59bf1c451
Igor Galinovskii <galinovskii95@gmail.com>
2025-04-07 23:39:11 +0300

    fix gheader

--- js/header.js
+++ js/header.js
@@ -1,24 +1,50 @@
+// let lastScrollPosition = 0;
+// const header = document.querySelector(".header");
+// const headerCol2 = document.querySelector(".header-col2");
+
+// function handleScroll() {
+//   const currentScrollPosition = window.scrollY;
+//   console.log(currentScrollPosition);
+//   if (currentScrollPosition > lastScrollPosition) {
+//     header.classList.add("hidden");
+//   } else {
+//     header.classList.remove("hidden");
+
+//     if (currentScrollPosition > 300) {
+//       header.classList.add("scrolled");
+//     } else {
+//       header.classList.remove("scrolled");
+//     }
+//   }
+//   if (currentScrollPosition === 300) {
+//     header.classList.remove("scrolled");
+//   }
+//   lastScrollPosition = currentScrollPosition;
+// }
+
+// window.addEventListener("scroll", () => {
+//   requestAnimationFrame(handleScroll);
+// });
 let lastScrollPosition = 0;
 const header = document.querySelector(".header");
-const headerCol2 = document.querySelector(".header-col2");
 
 function handleScroll() {
   const currentScrollPosition = window.scrollY;
-  console.log(currentScrollPosition);
-  if (currentScrollPosition > lastScrollPosition) {
+
+  const scrollingDown = currentScrollPosition > lastScrollPosition;
+
+  if (scrollingDown && currentScrollPosition > 50) {
     header.classList.add("hidden");
   } else {
     header.classList.remove("hidden");
-
-    if (currentScrollPosition > 300) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
   }
-  if (currentScrollPosition === 300) {
+
+  if (currentScrollPosition > 300) {
+    header.classList.add("scrolled");
+  } else {
     header.classList.remove("scrolled");
   }
+
   lastScrollPosition = currentScrollPosition;
 }
 
