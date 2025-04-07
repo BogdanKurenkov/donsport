9313e423c0dc08d0d4a5c35a977cf6ffe4aa587d
Igor Galinovskii <galinovskii95@gmail.com>
2025-04-07 23:08:26 +0300

    fix commit

--- js/header.js
+++ js/header.js
@@ -1,26 +1,25 @@
 let lastScrollPosition = 0;
-const header = document.querySelector('.header');
-const headerCol2 = document.querySelector('.header-col2');
+const header = document.querySelector(".header");
+const headerCol2 = document.querySelector(".header-col2");
 
 function handleScroll() {
-    const currentScrollPosition = window.scrollY;
+  const currentScrollPosition = window.scrollY;
+  console.log(currentScrollPosition);
+  if (currentScrollPosition > lastScrollPosition) {
+    header.classList.add("hidden");
+  } else {
+    header.classList.remove("hidden");
 
-    if (currentScrollPosition > lastScrollPosition) {
-        header.classList.add('hidden');
+    if (currentScrollPosition > 100) {
+      header.classList.add("scrolled");
     } else {
-        header.classList.remove('hidden');
-
-        if (currentScrollPosition > 0) {
-            header.classList.add('scrolled')
-        } else {
-            header.classList.remove('scrolled')
-        }
+      header.classList.remove("scrolled");
     }
+  }
 
-    lastScrollPosition = currentScrollPosition;
+  lastScrollPosition = currentScrollPosition;
 }
 
-window.addEventListener('scroll', () => {
-    requestAnimationFrame(handleScroll);
+window.addEventListener("scroll", () => {
+  requestAnimationFrame(handleScroll);
 });
-
